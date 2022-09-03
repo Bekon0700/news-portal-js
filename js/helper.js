323b320e2116726e719241263c26d694edb6501c
Emdadul Islam <bekon1700@gmail.com>
2022-09-03 16:52:11 +0600

    sorting added

--- js/helper.js
+++ js/helper.js
@@ -19,7 +19,6 @@ const hideSpinner = () => {
 }
 
 const getSortOptions = (list, opt) => {
-    console.log(list, opt)
     if(list.length){
         if(opt === 'ascending'){
             return list.sort(function (a, b) {
@@ -32,5 +31,40 @@ const getSortOptions = (list, opt) => {
         }
     }
 
+}
+
+const contentCreator = (news) => {
+    const newsDiv = document.getElementById('items_list')
+    newsDiv.innerHTML = ``
+    news.forEach(item => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const time = new Date(item.author.published_date)
+        const date = time.toLocaleDateString('en-US', options)
+        const newsDetailUrl = `https://openapi.programming-hero.com/api/news/${item._id}`
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <div class=" bg-white rounded-2xl py-6 pl-6 pr-8 flex gap-6" onclick="loadModalINfo('${newsDetailUrl}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <img src="${item.image_url}" alt="" class="w-[400px] h-[400px] rounded-2xl">
+                <div class="flex flex-col justify-evenly">
+                    <p class="text-3xl font-bold">${item.title}</p>
+                    <p class=" truncate text-md text-gray-400 h-48 w-[900px]">${item.details}</p>
+                    <div class="flex justify-between">
+                        <div class="flex items-center gap-3">
+                            <img src="${item.author.img}" alt="" class="w-12 h-12 rounded-full">
+                            <div>
+                                <p>${item.author.name ? item.author.name : 'No value found'}</p>
+                                <p class="text-gray-400">${date}</p>
+                            </div>
+                        </div>
+                        <div class="text-lg font-semibold text-black flex items-center">
+                            <p>view: ${item.total_view ? item.total_view + ' million' : 'No value found'}</p>
+                        </div>
+                        <button class="px-3 py-1 rounded-lg bg-blue-400 text-white">View More</button>
+                    </div>
+                </div>
+            </div>
+        `
+        newsDiv.appendChild(div)
+    })
 }
 
--- js/main.js
+++ js/main.js
@@ -40,45 +40,16 @@ const loadSpecificNews = async (url, category) => {
 const showSpecificNews = (news, category) => {
     const itemDiv = document.getElementById('item_found')
     itemDiv.innerHTML = news.length ? `<p>${news.length} items found for category ${category} </p>` : `<p>Nothing found for category ${category}</p>`
-    const newsDiv = document.getElementById('items_list')
-    newsDiv.innerHTML = ``
+    
 
     const selector = document.getElementById('sort-option')
     getSortOptions(news, selector.value)
+    contentCreator(news)
     selector.addEventListener('change', (e)=> {
         getSortOptions(news, e.target.value)
+        contentCreator(news)
     })
 
-    news.forEach(item => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const time = new Date(item.author.published_date)
-        const date = time.toLocaleDateString('en-US', options)
-        const newsDetailUrl = `https://openapi.programming-hero.com/api/news/${item._id}`
-        const div = document.createElement('div')
-        div.innerHTML = `
-            <div class=" bg-white rounded-2xl py-6 pl-6 pr-8 flex gap-6" onclick="loadModalINfo('${newsDetailUrl}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img src="${item.image_url}" alt="" class="w-[400px] h-[400px] rounded-2xl">
-                <div class="flex flex-col justify-evenly">
-                    <p class="text-3xl font-bold">${item.title}</p>
-                    <p class=" truncate text-md text-gray-400 h-48 w-[900px]">${item.details}</p>
-                    <div class="flex justify-between">
-                        <div class="flex items-center gap-3">
-                            <img src="${item.author.img}" alt="" class="w-12 h-12 rounded-full">
-                            <div>
-                                <p>${item.author.name ? item.author.name : 'No value found'}</p>
-                                <p class="text-gray-400">${date}</p>
-                            </div>
-                        </div>
-                        <div class="text-lg font-semibold text-black flex items-center">
-                            <p>view: ${item.total_view ? item.total_view + ' million' : 'No value found'}</p>
-                        </div>
-                        <button class="px-3 py-1 rounded-lg bg-blue-400 text-white">View More</button>
-                    </div>
-                </div>
-            </div>
-        `
-        newsDiv.appendChild(div)
-    })
 }
 
 
